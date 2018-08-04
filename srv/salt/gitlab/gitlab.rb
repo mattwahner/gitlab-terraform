bf660d4031c70d169084c40f3463ec25dfcb2e01
Matthew Wahner <mattwahner@gmail.com>
2018-08-04 19:25:07 -0400

    Added registry config

--- srv/salt/gitlab/gitlab.rb
+++ srv/salt/gitlab/gitlab.rb
@@ -1,3 +1,8 @@
 external_url 'https://gitlab.mattwahner.com:8000'
 letsencrypt['enable'] = true
 letsencrypt['contact_emails'] = ['mattwahner@gmail.com']
+
+registry['enable'] = true
+registry_external_url 'https://gitlab.mattwahner.com:4567'
+registry_nginx['ssl_certificate'] = '/etc/gitlab/ssl/gitlab.mattwahner.com.crt'
+registry_nginx['ssl_certificate_key'] = '/etc/gitlab/ssl/gitlab.mattwahner.com.key'
